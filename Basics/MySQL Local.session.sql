7c423779a483a3a06e0d596cb82d4e87e16be480
Ahmadraza <razakhan.srt786@gmail.com>
2025-02-18 22:49:39 +0530

    sql commit

---
+++ Basics/MySQL Local.session.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE abc;
+SHOW DATABASES;
+
+
+use abc;
+
+CREATE TABLE abc(name VARCHAR(25), age INT, roll_no INT);
+INSERT INTO abc VALUES("Ahmad Raza Khan", 21, 60);
+
+SELECT * FROM abc;
