fec53c6937d4785dde0334a90fe2d2da9af4134f
Ahmadraza <razakhan.srt786@gmail.com>
2025-02-23 10:16:00 +0530

    new commit

---
+++ Basics/CREATE DATABASE university;.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE university;
+
+use university;
+
+CREATE TABLE xyz (name VARCHAR(50), year date);
+SHOW TABLES;
+
+INSERT INTO xyz VALUES(
+    'surajmal',
+    '2001-02-14'
+);
+
+INSERT INTO xyz VALUES(
+    'surajmalwa',
+    '2001-02-14'
+);
+
+SELECT * from xyz;
