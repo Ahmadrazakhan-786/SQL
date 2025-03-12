ad2b1c0e7b92897390e6ba1bda67fb4e15c10670
Ahmadraza <razakhan.srt786@gmail.com>
2025-03-12 16:10:17 +0530

    practices

---
+++ Practice/CREATE TABLE bachee(.sql
@@ -0,0 +1,21 @@
+CREATE TABLE bachee(
+    name VARCHAR(25),
+    marks INT,
+    grades VARCHAR(1)
+);
+
+SHOW TABLES;
+
+INSERT INTO bachee(name, marks, grades) VALUES 
+    ('ahmad', 95, 'A'),
+    ('ashir', 80, 'B'),
+    ('zaid', 81, 'B'),
+    ('ahshan', 87, 'B');
+
+SELECT * FROM bachee;
+
+ALTER TABLE bachee CHANGE COLUMN name full_name VARCHAR(25);
+
+DELETE FROM bachee WHERE marks<85;
+
+ALTER TABLE bachee DROP COLUMN grades;
