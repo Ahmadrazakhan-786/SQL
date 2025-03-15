3ecca11f731167eeca9c931b5cbb46dee5f8653c
Ahmadraza <razakhan.srt786@gmail.com>
2025-03-15 12:46:25 +0530

    SELF JOIN

--- Joins/CREATE DATABASE JOINS;.sql
+++ Joins/CREATE DATABASE JOINS;.sql
@@ -49,3 +49,34 @@ SELECT * FROM student AS s RIGHT JOIN course AS c ON s.id = c.id;
 -- FULL JOIN: Returns all records when there is a match in either left or right table
 
 SELECT * FROM student AS s LEFT JOIN course AS c ON s.id = c.id UNION SELECT * FROM student AS s RIGHT JOIN course AS c ON s.id = c.id;
+
+-- LEFT EXCUSIVE JOIN: Only left portion with no common data
+
+SELECT * FROM student AS a LEFT JOIN course AS b ON a.id = b.id WHERE b.id IS NULL;
+
+-- RIGHT EXCLUSIVE JOIN: Only right portion with no common data
+
+SELECT * FROM student AS a RIGHT JOIN course AS b ON a.id = b.id WHERE a.id IS NULL;
+
+-- Full EXCLUSIVE JOIN: returns left and right portion with no common data
+
+SELECT * FROM student AS a LEFT JOIN course AS b ON a.id = b.id WHERE b.id IS NULL UNION SELECT * FROM student AS a RIGHT JOIN course AS b ON a.id = b.id WHERE a.id IS NULL;
+
+
+-- SELF JOIN: It is a regular join but the table is joined with itself
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY,
+    name varchar(50),
+    manager_id INT
+);
+
+INSERT INTO employee(id, name, manager_id) VALUES
+    (101, 'adam', 103),
+    (102, 'bob', 104),
+    (103, 'casey', NULL),
+    (104, 'donald', 103);
+
+SELECT * FROM employee;
+
+SELECT a.name AS manager_name, b.name FROM employee AS a JOIN employee AS b ON a.id = b.manager_id;
